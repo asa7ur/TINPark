c9544b12cd12f9e4ea1f32ba9f133aeffb1c6a4b
Garik Asatryan <asa7ur@gmail.com>
2024-10-04 11:18:40 +0200

    Added a bunch of logos locally (for the moment)

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,33 @@
+import Audi from '../client/src/assets/Audi.png'
+import BMW from '../client/src/assets/BMW.png'
+import Citroen from '../client/src/assets/Citroen.png'
+import Default_2 from '../client/src/assets/Default_2.png'
+import Fiat from '../client/src/assets/Fiat.png'
+import Ford from '../client/src/assets/Ford.png'
+import Honda from '../client/src/assets/Honda.png'
+import Hyundai from '../client/src/assets/Hyundai.png'
+import Jaguar from '../client/src/assets/Jaguar.png'
+import Jeep from '../client/src/assets/Jeep.png'
+import KIA from '../client/src/assets/KIA.png'
+import Land_Rover from '../client/src/assets/Land_Rover.png'
+import Lexus from '../client/src/assets/Lexus.png'
+import Mazda from '../client/src/assets/Mazda.png'
+import Mercedes_Benz from '../client/src/assets/Mercedes_Benz.png'
+import Mini from '../client/src/assets/Mini.png'
+import Nissan from '../client/src/assets/Nissan.png'
+import Peugeot from '../client/src/assets/Peugeot.png'
+import Renault from '../client/src/assets/Renault.png'
+import Seat from '../client/src/assets/Seat.png'
+import Skoda from '../client/src/assets/Skoda.png'
+import Tesla from '../client/src/assets/Tesla.png'
+import Toyota from '../client/src/assets/Toyota.png'
+import Volkswagen from '../client/src/assets/Volkswagen.png'
+
 export const VEHICLE_BRAND = {
   AUDI: 'Audi',
   BMW: 'BMW',
   CITROEN: 'Citroën',
+  FIAT: 'Fiat',
   FORD: 'Ford',
   HONDA: 'Honda',
   HYUNDAI: 'Hyundai',
@@ -10,11 +36,13 @@ export const VEHICLE_BRAND = {
   KIA: 'Kia',
   LAND_ROVER: 'Land Rover',
   LEXUS: 'Lexus',
+  MAZDA: 'Mazda',
   MERCEDES_BENZ: 'Mercedes-Benz',
   MINI: 'Mini',
   NISSAN: 'Nissan',
   OPEL: 'Opel',
   PEUGEOT: 'Peugeot',
+  RANGE_ROVER: 'Range Rover',
   RENAULT: 'Renault',
   SEAT: 'Seat',
   SKODA: 'Skoda',
